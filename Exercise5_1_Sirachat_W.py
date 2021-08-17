ff1589d10e5392e64e2ceec1f25059e0b2f8a84a
fzperlie <76158958+fzperlie@users.noreply.github.com>
2021-08-17 14:40:59 +0700

    Added new exercise
    Exercise 5

---
+++ Exercise5_1_Sirachat_W.py
@@ -0,0 +1,13 @@
+m = float(input("Enter your first number : "))
+m2 = float(input("Enter your second number : "))
+r1 = (m+m2)
+r2 = (m-m2)
+r3 = (m*m2)
+r4 = (m/m2)
+print("Result")
+print(m,"+", m2,"= ", r1)
+print(m,"-", m2,"= ", r2)
+print(m,"*", m2,"= ", r3)
+print(m,"/", m2,"= ", r4)
+
+
---
+++ Exercise5_2_Sirachat_W.py
@@ -0,0 +1,5 @@
+x = int(input("Distance(Km) : "))
+z = float(input("Time (Hr) : "))
+v = x/z
+print("Speed = ",v,"Km/hr")
+
